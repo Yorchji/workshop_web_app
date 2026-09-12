52ca6e8e70b00c185752d66d687ecc7ac7a75b01
Tanawin Intarattanaphol <tanawin.i@ku.th>
2026-09-12 16:35:15 +0700

    .

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -26,7 +26,3 @@ var todos = new List<TodoGetDto>
 app.MapGet("/api/todos", () => Results.Ok(todos));
 
 app.Run();
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
