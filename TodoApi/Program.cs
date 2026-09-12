1d9f1d799427be1da37fff34934f7eb513a5fcde
Tanawin Intarattanaphol <tanawin.i@ku.th>
2026-09-12 16:35:09 +0700

    .

---
+++ TodoApi/Program.cs
@@ -0,0 +1,32 @@
+using TodoApi.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var todos = new List<TodoGetDto>
+{
+    new TodoGetDto(1, "Learn C#", true),
+    new TodoGetDto(2, "Build a web API", false),
+    new TodoGetDto(3, "Profit!", false)
+};
+
+app.MapGet("/api/todos", () => Results.Ok(todos));
+
+app.Run();
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
